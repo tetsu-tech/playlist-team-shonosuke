f08805702b211c7fd6e07ee168a7a073de2716b0
Kippei Wada <33307300+kip693@users.noreply.github.com>
2021-06-25 16:28:00 +0900

    feature/add_front2 (#15)
    * リセットしてhusky抜いた
    
    * Added Doc app

---
+++ front/src/Doc.tsx
@@ -0,0 +1,19 @@
+// import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="Doc">
+      <input className="Doc_input"></input>
+      <p>
+        This is Doc compornent
+      </p>
+      <button className="buttons">
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
